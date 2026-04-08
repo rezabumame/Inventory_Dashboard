1b9b10693927e4d76c3c79bd5bce0de9ed839438
Reza Mahendra <reza.mahendra@bumame.com>
2026-04-08 17:14:43 +0700

    chore(security): add CSP 'upgrade-insecure-requests' meta to prevent mixed content for AJAX redirects on HTTPS

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title><?= APP_NAME ?></title>
     <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="<?= base_url('assets/img/favicon.ico') ?>">
